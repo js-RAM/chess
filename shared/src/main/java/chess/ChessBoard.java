435b605c83303995367a4dbf3286648492602456
js-RAM <swartzjoshua99@gmail.com>
2024-02-02 00:29:51 -0700

    added some documentation

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -11,9 +11,12 @@ import java.util.HashSet;
  * signature of the existing methods.
  */
 public class ChessBoard {
+
+    private final int boardMin = 1;
+    private final int boardMax = 8;
     private ChessPiece[][] board;
     public ChessBoard() {
-        board = new ChessPiece[8][8];
+        board = new ChessPiece[boardMax][boardMax];
     }
 
     /**
@@ -26,12 +29,20 @@ public class ChessBoard {
         board[position.getColumn()-1][position.getRow()-1] =  piece;
     }
 
-    public void movePiece(ChessMove move) {
+  /**
+   * Implements a move on the board
+   * @param move The ChessMove to implement
+   */
+  public void movePiece(ChessMove move) {
       addPiece(move.getEndPosition(), getPiece(move.getStartPosition()));
       removePiece(move.getStartPosition());
     }
 
-    private void removePiece(ChessPosition position) {
+  /**
+   * Removes a piece at the specified position
+   * @param position The ChessPosition to remove a piece at
+   */
+  private void removePiece(ChessPosition position) {
       board[position.getColumn()-1][position.getRow()-1] = null;
     }
 
@@ -46,8 +57,17 @@ public class ChessBoard {
         return board[position.getColumn()-1][position.getRow()-1];
     }
 
-    public boolean isPiece(int posX, int posY) {
-        return board[posX-1][posY-1] != null;
+  /**
+   * Returns whether a piece exists at the location on the chessboard.
+   * Parameters must be between 1 and 8, otherwise it will always return false.
+   *
+   * @param col The column of the piece
+   * @param row The row of the piece
+   * @return if there is a piece there
+   */
+    public boolean isPiece(int col, int row) {
+        if (col < boardMin || col > boardMax || row < boardMin || row > boardMax) return false;
+        return board[col-1][row-1] != null;
     }
 
     /**
@@ -55,7 +75,7 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        board = new ChessPiece[8][8];
+        board = new ChessPiece[boardMax][boardMax];
         ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
         int row = 0;
         for (int i = 0; i < 2; i++){
@@ -83,8 +103,8 @@ public class ChessBoard {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ChessBoard that=(ChessBoard) o;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < boardMax; i++) {
+            for (int j = 0; j < boardMax; j++) {
                 if (board[i][j] == null && that.board[i][j] == null) continue;
                 if (!board[i][j].equals(that.board[i][j])) return false;
             }
