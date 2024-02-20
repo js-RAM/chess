c12d27e320925d794539482cdc0c38967a8e9160
js-RAM <swartzjoshua99@gmail.com>
2024-02-19 22:20:47 -0700

    created basic structure for the data access interface and class

--- server/src/main/java/dataAccess/DataAccessInterface.java
+++ server/src/main/java/dataAccess/DataAccessInterface.java
@@ -1,4 +1,29 @@
 package dataAccess;
 
+import model.*;
+
 public interface DataAccessInterface {
+    public UserData getUser(String username);
+
+    public UserData getUser(String username, String password);
+
+    public void addUser(UserData userData);
+
+    public void addAuth(AuthData authData);
+
+    public AuthData getAuth(String authToken);
+
+    public void deleteAuth(AuthData authData);
+
+    public GameData[] getGames(int gameID);
+
+    public void addGame(GameData gameData);
+
+    public void updateGame(int gameID, GameData newGameData);
+
+    public void clearUsers();
+
+    public void clearAuth();
+
+    public void clearGames();
 }
--- server/src/main/java/dataAccess/MemoryDataAccess.java
+++ server/src/main/java/dataAccess/MemoryDataAccess.java
@@ -1,4 +1,67 @@
 package dataAccess;
 
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
 public class MemoryDataAccess implements DataAccessInterface{
+    @Override
+    public UserData getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public UserData getUser(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public void addUser(UserData userData) {
+
+    }
+
+    @Override
+    public void addAuth(AuthData authData) {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(AuthData authData) {
+
+    }
+
+    @Override
+    public GameData[] getGames(int gameID) {
+        return new GameData[0];
+    }
+
+    @Override
+    public void addGame(GameData gameData) {
+
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData newGameData) {
+
+    }
+
+    @Override
+    public void clearUsers() {
+
+    }
+
+    @Override
+    public void clearAuth() {
+
+    }
+
+    @Override
+    public void clearGames() {
+
+    }
 }
