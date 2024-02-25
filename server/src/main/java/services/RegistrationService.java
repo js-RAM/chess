01cb470cb1a2ec42b416e14da0e66c9f3ae6c414
js-RAM <swartzjoshua99@gmail.com>
2024-02-24 23:43:35 -0700

    basic structure of RegistrationService

--- server/src/main/java/services/RegistrationService.java
+++ server/src/main/java/services/RegistrationService.java
@@ -4,4 +4,15 @@ package services;
  * Manages the registration service
  */
 public class RegistrationService {
+    public RegistrationService() {
+
+    }
+
+    public String register(String username, String password, String email) {
+        return "";
+    }
+
+    public boolean isValidEmail(String email) {
+        return true;
+    }
 }
