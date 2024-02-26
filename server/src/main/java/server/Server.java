1dcbc75affaf189193195076892983eeffc0c0a3
js-RAM <swartzjoshua99@gmail.com>
2024-02-25 23:15:03 -0700

    added more routes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import services.GameMgmtService;
+import services.LoginService;
 import services.RegistrationService;
 import spark.*;
 
@@ -9,14 +11,55 @@ public class Server {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
-        Spark.post("/user/:username/:password/:email", (req, res) -> {
-            RegistrationService registrationService = new RegistrationService();
+
+        RegistrationService registrationService = new RegistrationService();
+        LoginService loginService = new LoginService();
+        GameMgmtService gameMgmtService = new GameMgmtService();
+
+        // Register your endpoints and handle exceptions here.
+        Spark.post("/register/:username/:password/:email", (req, res) -> {
+
             String username = req.params(":username");
             String password = req.params(":password");
             String email = req.params(":email");
+            System.out.println(username + password + email);
             return registrationService.register(username, password, email);
         });
-        // Register your endpoints and handle exceptions here.
+
+        Spark.post("/session/:username/:password", (req, res) -> {
+            String username = req.params(":username");
+            String password = req.params(":password");
+            System.out.println(username + password);
+            return registrationService.register(username, password,"");
+        });
+
+        Spark.delete("/session/:authToken", (req, res) -> {
+            String authToken = req.params(":authToken");
+            System.out.println(authToken);
+            return registrationService.register(authToken, "", "");
+        });
+
+        Spark.get("/game/:authToken", (req, res) -> {
+            String authToken = req.params(":authToken");
+            return "";
+        });
+
+        Spark.post("/game/:authToken/:gameName", (req, res) -> {
+            String authToken = req.params(":authToken");
+            String gameName = req.params(":gameName");
+            return "";
+        });
+
+        Spark.put("/game/:authToken/:ClientColor/:gameID", (req, res) -> {
+            String authToken = req.params(":authToken");
+            String clientColor = req.params(":ClientColor");
+            String gameID = req.params(":gameID");
+            return "";
+        });
+
+        Spark.delete("/db", (req, res) -> {
+            return 200;
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
