c4098eb1997f1381dfdfbfb08e3404bc87188fe0
js-RAM <swartzjoshua99@gmail.com>
2024-02-25 23:39:52 -0700

    implemented RegistrationService

--- server/src/main/java/services/RegistrationService.java
+++ server/src/main/java/services/RegistrationService.java
@@ -1,18 +1,36 @@
 package services;
 
+import dataAccess.DataAccessInterface;
+import dataAccess.MemoryDataAccess;
+import model.AuthData;
+import model.UserData;
+
 /**
  * Manages the registration service
  */
 public class RegistrationService {
+    DataAccessInterface dataAccess;
     public RegistrationService() {
-
+        dataAccess = new MemoryDataAccess();
     }
 
-    public String register(String username, String password, String email) {
-        return "";
+    public String register(String username, String password, String email) throws ServicesException {
+        if (isValidEmail(email)) {
+            throw new ServicesException("400");
+        }
+        if (dataAccess.getUser(username) == null) {
+            throw new ServicesException("403");
+        }
+
+        UserData newUserData = new UserData(username, password, email);
+        dataAccess.addUser(newUserData);
+
+        String authToken = new AuthTokenGenerator().generateAuthToken();
+        dataAccess.addAuth(new AuthData(authToken, username));
+        return authToken;
     }
 
     public boolean isValidEmail(String email) {
-        return true;
+        return email.matches("(.*)@(.*).(.*)");
     }
 }
