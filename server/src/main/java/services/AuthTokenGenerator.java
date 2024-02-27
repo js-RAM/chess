26e59953fc0c96fd83d866952e66e7eb8d97fee6
js-RAM <swartzjoshua99@gmail.com>
2024-02-26 22:44:48 -0700

    created an AuthTokenGenerator

--- server/src/main/java/services/AuthTokenGenerator.java
+++ server/src/main/java/services/AuthTokenGenerator.java
@@ -1,9 +1,16 @@
 package services;
 
+import dataAccess.DataAccessInterface;
 import model.AuthData;
 
+import java.util.Random;
+
 public class AuthTokenGenerator {
-    public String generateAuthToken() {
-        return "";
+    public String generateAuthToken(DataAccessInterface dataAccess) {
+        String authToken;
+        do {
+            authToken = Integer.toString(new Random().nextInt(999999999));
+        } while (dataAccess.getAuth(authToken) != null);
+        return authToken;
     }
 }
