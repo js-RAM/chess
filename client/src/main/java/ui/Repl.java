93b33b81f2b2a86284d459957013b514ad03227f
js-RAM <swartzjoshua99@gmail.com>
2024-03-19 23:12:24 -0600

    added client and input to repl

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,13 +1,37 @@
 package ui;
 
+import java.util.Scanner;
+
 import static ui.EscapeSequences.*;
 
 public class Repl {
+    private Client client;
     public Repl(String url) {
-
+        client = new Client(url);
     }
 
     public void run() {
         System.out.println(SET_TEXT_COLOR_BLUE+"Welcome to Chess. Sign in to start.\nUse 'help' to see a list of commands.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
     }
 }
