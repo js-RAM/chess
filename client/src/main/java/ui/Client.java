0af7fe91400605af3e03a98931458e7478c50694
js-RAM <swartzjoshua99@gmail.com>
2024-03-20 22:43:13 -0600

    added logout and register to eval

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -3,14 +3,21 @@ package ui;
 import exception.ServerException;
 import java.util.Arrays;
 
+import model.AuthData;
 import model.LoginRequest;
+import model.UserData;
 import server.ServerFacade;
 
 public class Client {
 
     private ServerFacade serverFacade;
+    private AuthData authData;
+    private LoginRequest loginRequest;
+
     public Client(String url) {
         serverFacade = new ServerFacade(url);
+        authData = null;
+        loginRequest = null;
     }
 
     public String eval(String input) {
@@ -19,7 +26,9 @@ public class Client {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
+                case "register" -> register(params);
                 case "signin" -> signIn(params);
+                case "signout" -> signOut();
                 case "quit" -> "quit";
                 default -> help();
             };
@@ -28,15 +37,35 @@ public class Client {
         }
     }
 
+    public String register(String... params) throws ServerException {
+        if (params.length >= 3) {
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            loginRequest = new LoginRequest(username, password);
+            authData = serverFacade.register(new UserData(username, password, email));
+
+            return String.format("You signed in as %s.", username);
+        }
+        throw new ServerException(400, "Expected: <username> <password> <email>");
+    }
+
     public String signIn(String... params) throws ServerException {
         if (params.length >= 2) {
             String username = params[0];
             String password = params[1];
-            serverFacade.login(new LoginRequest(username, password));
+            loginRequest = new LoginRequest(username, password);
+            authData = serverFacade.login(loginRequest);
 
             return String.format("You signed in as %s.", username);
         }
         throw new ServerException(400, "Expected: <username> <password>");
+    }
+
+    public String signOut() throws ServerException {
+        serverFacade.logout(loginRequest);
+
+        return "You signed out!";
     }
 
     public String help() {
